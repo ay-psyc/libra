036fd1ef2b42909b516269d0cecb145a0f1c8aab
ay-psyc <asy.psyc@gmail.com>
2026-06-14 23:26:34 +0530

    feat(auth): implement reset password UI

---
+++ src/app/features/auth/pages/reset-password/reset-password.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { InputText } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-reset-password',
+  imports: [InputText],
+  templateUrl: './reset-password.html',
+  styleUrl: './reset-password.css',
+})
+export class ResetPassword {}
