bc65adee4a8bd3b29c0581f2320c02e8544e7071
ay-psyc <asy.psyc@gmail.com>
2026-06-14 23:19:34 +0530

    feat(auth): implement OTP verification UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
 import { Register } from './features/auth/pages/register/register';
 import { ForgotPassword } from './features/auth/pages/forgot-password/forgot-password';
+import { VerifyOtp } from './features/auth/pages/verify-otp/verify-otp';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Register, ForgotPassword],
+  imports: [RouterOutlet, Login, Register, ForgotPassword, VerifyOtp],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/features/auth/pages/verify-otp/verify-otp.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { InputText } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-verify-otp',
+  imports: [InputText],
+  templateUrl: './verify-otp.html',
+  styleUrl: './verify-otp.css',
+})
+export class VerifyOtp {}
