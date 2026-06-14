f13482eb76c300a838b3771040933b2d3b7cdb5c
ay-psyc <asy.psyc@gmail.com>
2026-06-14 23:13:56 +0530

    feat(auth): implement forgot password UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
 import { Register } from './features/auth/pages/register/register';
+import { ForgotPassword } from './features/auth/pages/forgot-password/forgot-password';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Register],
+  imports: [RouterOutlet, Login, Register, ForgotPassword],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/features/auth/pages/forgot-password/forgot-password.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { InputText } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-forgot-password',
+  imports: [InputText],
+  templateUrl: './forgot-password.html',
+  styleUrl: './forgot-password.css',
+})
+export class ForgotPassword {}
